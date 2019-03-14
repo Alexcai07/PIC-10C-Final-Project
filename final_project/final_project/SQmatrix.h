5a8e12df7933283f1c1291f2da99a34cb4714f2a
Haocheng Cai <caihaocheng07@hotmail.com>
2019-03-14 15:12:22 -0700

    create declaration of matrix class

--- final_project/final_project/SQmatrix.h
+++ final_project/final_project/SQmatrix.h
@@ -1,1 +1,38 @@
-#pragma once
+#ifndef SQMATRIX_H
+#define SQMATRIX_H
+#include<vector>
+
+
+class  SQmatrix {
+private:
+	std::vector<std::vector<int>> matrix;
+	int sz;
+
+public:
+	SQmatrix();
+
+	SQmatrix(const SQmatrix &m);
+
+	void print()const;
+
+	SQmatrix operator+(const SQmatrix& rhs);
+	SQmatrix& operator+=(const SQmatrix& rhs);
+	SQmatrix operator-(const SQmatrix& rhs);
+	SQmatrix& operator-=(const SQmatrix& rhs);
+	SQmatrix operator*(const SQmatrix& rhs);
+	SQmatrix& operator*=(const SQmatrix& rhs);
+
+	SQmatrix operator*(const SQmatrix& rhs);
+	SQmatrix power(int a);
+};
+
+class graph {
+private:
+
+};
+
+
+
+
+#endif 
+
