c62b40ea3b0496665b71d7e0aa65564276ed5bb8
Haocheng Cai <caihaocheng07@hotmail.com>
2019-03-14 16:41:04 -0700

    declare graph class and node class, and let them make friends

--- final_project/final_project/SQmatrix.h
+++ final_project/final_project/SQmatrix.h
@@ -1,7 +1,7 @@
 #ifndef SQMATRIX_H
 #define SQMATRIX_H
 #include<vector>
-
+#include<unordered_set>
 
 class  SQmatrix {
 private:
@@ -24,10 +24,24 @@ public:
 
 	SQmatrix operator*(const SQmatrix& rhs);
 	SQmatrix power(int a);
+
+	friend graph;
+	
 };
 
 class graph {
 private:
+	
+	class node {
+	public:
+		std::vector<node> connection;
+		friend graph;
+		friend SQmatrix;
+	};
+
+	std::unordered_set<node> nodes;
+	SQmatrix M;
+
 
 };
 
